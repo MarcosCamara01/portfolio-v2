e1beba210c236b310eee69cbf5240d06f57c96b5
MarcosCamara01 <marcospenelascamara@gmail.com>
2023-07-18 18:48:57 +0200

    CV, new images and new project

--- src/components/common/SocialMedia.jsx
+++ src/components/common/SocialMedia.jsx
@@ -9,10 +9,10 @@ export const SocialMedia = () => {
     return (
         <div className='socialmedia'>
             <ul>
-                <li><a href="https://github.com/MarcosCamara01" target="_blank" ><FiGithub /></a></li>
+                <li><a href="https://github.com/MarcosCamara01" target="_blank"><FiGithub /></a></li>
                 <li><a href="#"><FiInstagram /></a></li>
-                <li><a href="#"><FiTwitter /></a></li>
-                <li><a href="https://www.linkedin.com/in/marcos-penelas-a15490255/" target="_blank"><FiLinkedin /></a></li>
+                <li><a href='./pdf/CurriculumVitae.pdf' target="_blank"><span>CV</span></a></li>
+                <li><a href="https://www.linkedin.com/in/marcospenelascamara/" target="_blank"><FiLinkedin /></a></li>
                 <li><a href="https://codepen.io/MarcosPenelas" target="_blank"><FiCodepen /></a></li>
             </ul>
         </div>
