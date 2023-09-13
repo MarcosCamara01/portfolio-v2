597557c9e459b51f458ba5c3b90b6f67db70be52
MarcosCamara01 <marcospenelascamara@gmail.com>
2023-09-13 23:51:08 +0200

    detect language automatically

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
 import { I18nextProvider } from "react-i18next";
 import i18next from 'i18next';
@@ -10,29 +10,53 @@ import global_en from "./translations/en/global.json";
 
 i18next.init({
   interpolation: { escapeValue: false },
-  lng: "es",
-  fallbackLng: 'es',
+  lng: getDefaultLanguage(),
+  fallbackLng: 'en',
   resources: {
-    es:{
+    es: {
       global: global_es
     },
     en: {
       global: global_en
     }
   }
-})
+});
 
-const handleLanguageChange = () => {
+function getDefaultLanguage() {
+  const userLanguage = navigator.language || "es";
+
+  if (userLanguage === "es" || userLanguage === "en") {
+    return userLanguage;
+  } else {
+    return "en";
+  }
+}
+
+const handleLanguageChange = (newLanguage) => {
   const currentLanguage = i18next.language;
-  document.documentElement.lang = currentLanguage;
+  if (currentLanguage !== newLanguage) {
+    i18next.changeLanguage(newLanguage);
+  }
+  document.documentElement.lang = newLanguage;
 };
 
 i18next.on('languageChanged', handleLanguageChange);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <I18nextProvider i18n={i18next}>
-      <App />
-    </I18nextProvider>
-  </React.StrictMode>,
-)
+const Root = () => {
+  useEffect(() => {
+    const currentLanguage = i18next.language;
+    if (currentLanguage !== navigator.language) {
+      handleLanguageChange(navigator.language);
+    }
+  }, []);
+
+  return (
+    <React.StrictMode>
+      <I18nextProvider i18n={i18next}>
+        <App />
+      </I18nextProvider>
+    </React.StrictMode>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Root />);
